3933264705b8993d7adab9a34ead19b8e29a0aaa
ramilnurgaleev <nurgaleev.dev.tat@gmail.com>
2024-12-01 21:50:15 +0300

    initial

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ window = Tk()
 window.wm_attributes('-alpha', 0.93)
 
 window.title("Создание документов для квестов")
-window.geometry('1080x730')
+window.geometry('1200x800')
 window.resizable(width=False, height=False)
 
 frame = Frame(window)
